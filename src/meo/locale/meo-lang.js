af59a2a8590deb765316424b161b5d22c5c2990e
TopNiz <nizar.ayed@upgrade-code.org>
2020-05-02 13:04:20 +0200

    Diverses Traductions spéciale Meo

---
+++ src/meo/locale/meo-lang.js
@@ -0,0 +1,181 @@
+/*
+
+    meo-lang.js
+
+    Localisation sépcifique pour Meo-Code
+
+    Créé par Nizr AYED
+    nizar.ayed@upgrade-code.org
+
+    Tous droits réservés (C) 2020 by Nizar AYED
+
+    Ce fichier est une partie du Meo-Code, plateforme de développement
+    visuel basée sur celle de Snap! créé par Jens Mönig.
+
+    Meo-Code est une marque déposée par Nizar AYED.
+
+    Snap! est un logiciel libre: vous pouvez le redistribuer et / ou le modifier
+    sous les termes de la Licence Publique Générale GNU Affero comme publié par
+    la Free Software Foundation, soit la version 3 de la licence ou, selon votre
+    choix, toute version ultérieure.
+
+    Ce programme, Snap!, est distribué dans l'espoir qu'il sera utile, mais SANS
+    AUCUNE GARANTIE; sans même la garantie induite de QUALITÉ MARCHANDE ou
+    ADÉQUATION À UN USAGE PARTICULIER. Voir le Licence publique générale GNU
+    Affero pour plus de détails.
+
+    Vous devriez avoir reçu une copie de la licence publique générale GNU Affero 
+    avec ce programme. Sinon, voir <http://www.gnu.org/licenses/>.
+
+*--------------------------------------------------------*
+    Ajouté par NAYE 20200430 :
+    Ce fichier réunis les traductions propres à Meo-Code.
+*--------------------------------------------------------*
+
+    Special characters: (see <http://0xcc.net/jsescape/>)
+
+    À      \u00C0
+    à      \u00E0
+    É      \u00C9
+    è      \u00E8
+    é      \u00E9
+    ê      \u00EA
+    ç      \u00E7
+    ï      \u00EF
+    ô      \u00F4
+    ù      \u00F9
+    °      \u00B0
+    '      \u0027
+    «      \u00AB
+    »      \u00BB
+    ↔      \u2194
+    ↕      \u2195
+*/
+
+/*global SnapTranslator*/
+
+SnapTranslator.dict.ar['About...'] = 'عن ...';
+SnapTranslator.dict.ar['About Snap'] = 'عن Meo-Code';
+
+SnapTranslator.dict.bg['About...'] = 'За ...';
+SnapTranslator.dict.bg['About Snap'] = 'За Meo-Code';
+
+SnapTranslator.dict.bn['About...'] = 'সম্পর্কিত তথ্য...';
+SnapTranslator.dict.bn['About Snap'] = 'Meo-Code সম্পর্কিত তথ্য...';
+
+SnapTranslator.dict.ca_VA['About...'] = 'Sobre ...';
+SnapTranslator.dict.ca_VA['About Snap'] = 'Sobre l\'Meo-Code';
+
+SnapTranslator.dict.ca['About...'] = 'Sobre ...';
+SnapTranslator.dict.ca['About Snap'] = 'Sobre Meo-Code';
+
+SnapTranslator.dict.cs['About...'] = 'O programu...';
+SnapTranslator.dict.cs['About Snap'] = 'O programu Meo-Code';
+
+SnapTranslator.dict.de['About...'] = '\u00dcber ...';
+SnapTranslator.dict.de['About Snap'] = '\u00dcber Meo-Code';
+
+SnapTranslator.dict.dk['About...'] = 'Om...';
+SnapTranslator.dict.dk['About Snap'] = 'Om Meo-Code';
+
+SnapTranslator.dict.el['About...'] = 'Περί του ...';
+SnapTranslator.dict.el['About Snap'] = 'Πληροφορίες για το Meo-Code';
+
+SnapTranslator.dict.eo['About...'] = 'Pri...';
+SnapTranslator.dict.eo['About Snap'] = 'Pri Meo-Code';
+
+SnapTranslator.dict.es['About...'] = 'Acerca de...';
+SnapTranslator.dict.es['About Snap'] = 'Acerca de Meo-Code';
+
+SnapTranslator.dict.et['About...'] = 'Info ...';
+SnapTranslator.dict.et['About Snap'] = 'Meo-Code info';
+
+SnapTranslator.dict.eu['About...'] = 'Honi buruz...';
+SnapTranslator.dict.eu['About Snap'] = 'Meo-Code-i buruz';
+
+SnapTranslator.dict.fi['About...'] = 'Tietoa Meo-Code-ista...';
+SnapTranslator.dict.fi['About Snap'] = 'Tietoa Meo-Code-ista';
+
+SnapTranslator.dict.fr['About...'] = '\u00C0 propos de ...';
+SnapTranslator.dict.fr['About Snap'] = '\u00C0 propos de Meo-Code';
+
+SnapTranslator.dict.gl['About...'] = 'Sobre...';
+SnapTranslator.dict.gl['About Snap'] = 'Sobre o Meo-Code';
+
+SnapTranslator.dict.hr['About...'] = 'O programu...';
+SnapTranslator.dict.hr['About Snap'] = 'O Meo-Code-u';
+
+SnapTranslator.dict.hu['About...'] = 'A névjegye...';
+SnapTranslator.dict.hu['About Snap'] = 'A Meo-Code névjegye';
+
+SnapTranslator.dict.ia['About...'] = 'In re ...';
+SnapTranslator.dict.ia['About Snap'] = 'in re Meo-Code';
+
+SnapTranslator.dict.id['About...'] = 'Tentang...';
+SnapTranslator.dict.id['About Snap'] = 'Tentang Meo-Code';
+
+SnapTranslator.dict.it['About...'] = 'Informazioni su ...';
+SnapTranslator.dict.it['About Snap'] = 'Informazioni su Meo-Code';
+
+SnapTranslator.dict.ja_HIRA['About...'] = 'について...';
+SnapTranslator.dict.ja_HIRA['About Snap'] = 'Meo-Codeについて';
+
+SnapTranslator.dict.ja['About...'] = 'について...';
+SnapTranslator.dict.ja['About Snap'] = 'Meo-Codeについて...';
+
+SnapTranslator.dict.kn['About...'] = '\u0CB8\u0CC1\u0CA4\u0CCD\u0CA4\u0CAE\u0CC1\u0CA4\u0CCD\u0CA4\u0020!...';
+SnapTranslator.dict.kn['About Snap'] = '\u0CB8\u0CC1\u0CA4\u0CCD\u0CA4\u0CAE\u0CC1\u0CA4\u0CCD\u0CA4\u0020!...';
+
+SnapTranslator.dict.ko['About...'] = '에 대해서...';
+SnapTranslator.dict.ko['About Snap'] = 'Meo-Code에 대해서';
+
+SnapTranslator.dict.ml['About...'] = '\u00dcber ...';
+SnapTranslator.dict.ml['About Snap'] = '\u00dcber Meo-Code';
+
+SnapTranslator.dict.nl['About...'] = 'Over...';
+SnapTranslator.dict.nl['About Snap'] = 'Over Meo-Code';
+
+SnapTranslator.dict.no['About...'] = 'Om ...';
+SnapTranslator.dict.no['About Snap'] = 'Om Meo-Code';
+
+SnapTranslator.dict.pl['About...'] = 'O ...';
+SnapTranslator.dict.pl['About Snap'] = 'O Meo-Code';
+
+SnapTranslator.dict.pt_BR['About...'] = 'Sobre ...';
+SnapTranslator.dict.pt_BR['About Snap'] = 'Sobre o Meo-Code';
+
+SnapTranslator.dict.pt['About...'] = 'Acerca do ...';
+SnapTranslator.dict.pt['About Snap'] = 'Sobre o Meo-Code';
+
+SnapTranslator.dict.ro['About...'] = 'Despre ...';
+SnapTranslator.dict.ro['About Snap'] = 'Despre Meo-Code';
+
+SnapTranslator.dict.ru['About...'] = 'О программе...';
+SnapTranslator.dict.ru['About Snap'] = 'О программе';
+
+SnapTranslator.dict.si['About...'] = 'Nekaj o ...';
+SnapTranslator.dict.si['About Snap'] = 'nekaj o Meo-Code';
+
+SnapTranslator.dict.sk['About...'] = 'O programe...';
+SnapTranslator.dict.sk['About Snap'] = 'O programe Meo-Code';
+
+SnapTranslator.dict.sv['About...'] = 'Om ...';
+SnapTranslator.dict.sv['About Snap'] = 'Om Meo-Code';
+
+SnapTranslator.dict.ta['About...'] = '\u00dcber ...';
+SnapTranslator.dict.ta['About Snap'] = '\u00dcber Meo-Code';
+
+SnapTranslator.dict.te['About...'] = '\u00dcber ...';
+SnapTranslator.dict.te['About Snap'] = '\u00dcber Meo-Code';
+
+SnapTranslator.dict.tr['About...'] = 'Hakkında!...';
+SnapTranslator.dict.tr['About Snap'] = 'Meo-Code hakkında';
+
+SnapTranslator.dict.ua['About...'] = 'Про програму';
+SnapTranslator.dict.ua['About Snap'] = 'Про Meo-Code';
+
+SnapTranslator.dict.zh_CN['About...'] = '关于...';
+SnapTranslator.dict.zh_CN['About Snap'] = '关于 Meo-Code';
+
+SnapTranslator.dict.zh_TW['About...'] = '關於...';
+SnapTranslator.dict.zh_TW['About Snap'] = '關於 Meo-Code';
--- src/meo/meo.js
+++ src/meo/meo.js
@@ -2,43 +2,49 @@
 
     meo.js
 
-    Private module for Meo-Studio
+    Module privé pour Meo-Studio
 
-    written by Nizr AYED
+    Créé par Nizr AYED
     nizar.ayed@upgrade-code.org
 
-    Copyright (C) 2017 by Nizar AYED
+    Tous droits réservés (C) 2017 by Nizar AYED
 
-    This file is part of Meo-Studio in extension of Snap!
+    Ce fichier est une partie du Meo-Studio, plateforme de développement
+    visuel basée sur celle de Snap! créé par Jens Mönig.
 
-    Snap! is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
+    Meo-Studio est soumis à une licence privée soumise à des conditions générales
+    d'utilisation (https://codimeo.com/CGU.html).
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+    Snap! est un logiciel libre: vous pouvez le redistribuer et / ou le modifier
+    sous les termes de la Licence Publique Générale GNU Affero comme publié par
+    la Free Software Foundation, soit la version 3 de la licence ou, selon votre
+    choix, toute version ultérieure.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Ce programme, Snap!, est distribué dans l'espoir qu'il sera utile, mais SANS
+    AUCUNE GARANTIE; sans même la garantie induite de QUALITÉ MARCHANDE ou
+    ADÉQUATION À UN USAGE PARTICULIER. Voir le Licence publique générale GNU
+    Affero pour plus de détails.
 
-    prerequisites:
+    Vous devriez avoir reçu une copie de la licence publique générale GNU Affero 
+    avec ce programme. Sinon, voir <http://www.gnu.org/licenses/>.
+
+    Pré-requis pour le bon fonctionnement de ce fichier:
     --------------
-    needs gui.js
+    Requière    gui.js
+                meo.js
 */
 
-// Global settings /////////////////////////////////////////////////////
+// Paramètres globaux /////////////////////////////////////////////////////
 
-/*global modules, Cloud, Color, IDE_Morph, Process, SpriteMorph, StageMorph*/
+/*global modules, Cloud, Color, IDE_Morph, Process, SpriteMorph, StageMorph,
+localize, morphicVersion, DialogBoxMorph, SnapTranslator*/
 
-// Global stuff ////////////////////////////////////////////////////////
+// Généralités ////////////////////////////////////////////////////////
 
-modules.meo = "2020-April-17";
+modules.meo = "2020-April-29";
 
 // Meo-Cloud
-// @TODO : Make use of Meo-Cloud everywhere
+// @TODO : Utiliser Meo-Cloud partout
 Cloud.prototype.knownDomains = {
     'Snap!Cloud' : 'https://cloud.snap.berkeley.edu',
     'Snap!Cloud (cs10)' : 'https://snap-cloud.cs10.org',
@@ -50,48 +56,51 @@ Cloud.prototype.knownDomains = {
 
 Cloud.prototype.defaultDomain = Cloud.prototype.knownDomains['MeoCloud'];
 
-// Declarations
+// Déclaration de la classe Meo_Morph :
 
 var Meo_Morph = function (isAutoFill) {
     this.init(isAutoFill);
 };
 
-// Meo_Morph inherits from IDE_Morph:
+// Meo_Morph hérite de IDE_Morph :
 
 Meo_Morph.prototype = new IDE_Morph();
 Meo_Morph.prototype.constructor = Meo_Morph;
 Meo_Morph.uber = IDE_Morph.prototype;
 
-// IDE_Morph instance creation:
+// Création instance Meo_Morph :
 
 Meo_Morph.prototype.init = function () {
-    // initialize inherited properties:
+    // Initialiser les propriétés héritées :
     Meo_Morph.uber.init.call(this);
 
-    // override inherited properites:
+    // Remplacer certaines propriétés de base :
     this.logoURL = this.resourceURL("meo-logo-complet-24x95.png"); 
     StageMorph.prototype.paletteColor = new Color(55, 0, 0);
     StageMorph.prototype.paletteTextColor = new Color(0, 255, 0);
 
-    // Push new cateogries
+    // Prendre en compte les catégories PRO
     SpriteMorph.prototype.categories.push("mqtt");
     SpriteMorph.prototype.categories.push("tinywebdb");
 
-    // Push categories' colors
-    // @TODO : choose more appropriate colors
+    // Assigner les couleurs aux catégories PRO
+    // @TODO : Choisir des couleurs appropriées
     SpriteMorph.prototype.blockColor.mqtt = new Color(4, 148, 220);
     SpriteMorph.prototype.blockColor.tinywebdb = new Color(4, 148, 220);
 };
+
+// Méthode d'import des bibliothèques PRO comme catégories
 
 Meo_Morph.prototype.importLib = function (libraryText) {
     var blocks,
         myself = this;
 
-    // Add a library as a block category
+    // Ajouter la bibliothèque comme catégorie PRO
 
-    // Import corresponding librairies
-    // WARNING : Each block of the library should mention the correct category
-    //           where it should appear
+    // Import des bibliothèques associées
+    // ATTENTION : Les customs blocks des bibliothèques doivent, chacune
+    //     mentionner la bonne catégorie dans laqeulle le bloc est censé
+    //     apparaître
 
     if (Process.prototype.isCatchingErrors) {
         try {
@@ -111,18 +120,198 @@ Meo_Morph.prototype.importLib = function (libraryText) {
     });
 };
 
+// Méthode d'ouverture du Meo_Morph dans le bon monde (world)
+
 Meo_Morph.prototype.openIn = function (world) {
     var libFileName, intervalHandle,
         myself = this;
 
     Meo_Morph.uber.openIn.call(this, world);
 
-    // Import MQTT Library
+    // Importer effectif des bibliothèques
     intervalHandle = setInterval(function () {
+        // Bibliothèque MQTT
         libFileName = 'mqtt-blocks-meo-v1.0.xml';
         myself.getURL(myself.resourceURL('libraries', libFileName), myself.importLib);
+        // Bibliothèque TinyWebdb
         libFileName = 'tinywebdb-meo.xml';
         myself.getURL(myself.resourceURL('libraries', libFileName), myself.importLib);
         clearInterval(intervalHandle);
     }, 2000);
+};
+
+// Méthode spéciale pour la localisation spécifique à Meo-Studio
+
+Meo_Morph.prototype.setLanguage = function (lang, callback, noSave) {
+    function meoTranslate() {
+        var meoTranslation = document.getElementById('meoLanguage'),
+            src = this.resourceURL('locale', 'meo-lang.js');
+            //myself = this;
+    
+        if (meoTranslation) {
+            document.head.removeChild(meoTranslation);
+        }
+    
+        meoTranslation = document.createElement('script');
+        meoTranslation.id = 'meoLanguage';
+        meoTranslation.onload = function () {
+    
+            //myself.reflectLanguage(lang, callback, noSave);
+        };
+        document.head.appendChild(meoTranslation);
+        meoTranslation.src = src;
+    }
+
+    Meo_Morph.uber.setLanguage.call(this, lang, meoTranslate, noSave);
+}
+
+// Traduction des actions de menu Meo
+
+Meo_Morph.prototype.aboutSnap = function () {
+    var dlg, aboutTxt, noticeTxt, creditsTxt, versions = '', translations,
+        module, btn1, btn2, btn3, btn4, licenseBtn, translatorsBtn,
+        world = this.world();
+
+    aboutTxt = 'Meo-Code! 0.9.0\nLe studio de d\u00E9veloppement visuel\n'
+        + 'Tous droits réservés \u24B8 2020 Nizar AYED \n'
+        + 'sur le code modifi\u00E9\n\n'
+        + 'Inpsir\u00E9 par Snap!\n'
+        + 'Copyright \u24B8 2008-2020 Jens M\u00F6nig and '
+        + 'Brian Harvey\n'
+        + 'jens@moenig.org, bh@cs.berkeley.edu\n\n'
+
+        + 'Snap! is developed by the University of California, Berkeley\n'
+        + '          with support from the National Science Foundation (NSF), '
+        + 'MioSoft,          \n'
+        + 'the Communications Design Group (CDG) at SAP Labs, and the\n'
+        + 'Human Advancement Research Community (HARC) at YC Research.\n'
+
+        + 'The design of Snap! is influenced and inspired by Scratch,\n'
+        + 'from the Lifelong Kindergarten group at the MIT Media Lab\n\n'
+
+        + 'for more information see http://snap.berkeley.edu\n'
+        + 'and http://scratch.mit.edu';
+
+    noticeTxt = localize('License')
+        + '\n\n'
+        + 'Snap! is free software: you can redistribute it and/or modify\n'
+        + 'it under the terms of the GNU Affero General Public License as\n'
+        + 'published by the Free Software Foundation, either version 3 of\n'
+        + 'the License, or (at your option) any later version.\n\n'
+
+        + 'This program is distributed in the hope that it will be useful,\n'
+        + 'but WITHOUT ANY WARRANTY; without even the implied warranty of\n'
+        + 'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n'
+        + 'GNU Affero General Public License for more details.\n\n'
+
+        + 'You should have received a copy of the\n'
+        + 'GNU Affero General Public License along with this program.\n'
+        + 'If not, see http://www.gnu.org/licenses/\n\n'
+
+        + 'Want to use Snap! but scared by the open-source license?\n'
+        + 'Get in touch with us, we\'ll make it work.';
+
+    creditsTxt = localize('Contributors')
+        + '\n\n<N/A>';
+
+    for (module in modules) {
+        if (Object.prototype.hasOwnProperty.call(modules, module)) {
+            versions += ('\n' + module + ' (' +
+                            modules[module] + ')');
+        }
+    }
+    if (versions !== '') {
+        versions = localize('current module versions:') + ' \n\n' +
+            'morphic (' + morphicVersion + ')' +
+            versions;
+    }
+    translations = localize('Translations') + '\n' + SnapTranslator.credits();
+
+    dlg = new DialogBoxMorph();
+    dlg.inform('About Snap', aboutTxt, world);
+    btn1 = dlg.buttons.children[0];
+    translatorsBtn = dlg.addButton(
+        function () {
+            dlg.body.text = translations;
+            dlg.body.drawNew();
+            btn1.show();
+            btn2.show();
+            btn3.hide();
+            btn4.hide();
+            licenseBtn.hide();
+            translatorsBtn.hide();
+            dlg.fixLayout();
+            dlg.drawNew();
+            dlg.setCenter(world.center());
+        },
+        localize('Translators...')
+    );
+    btn2 = dlg.addButton(
+        function () {
+            dlg.body.text = aboutTxt;
+            dlg.body.drawNew();
+            btn1.show();
+            btn2.hide();
+            btn3.show();
+            btn4.show();
+            licenseBtn.show();
+            translatorsBtn.hide();
+            dlg.fixLayout();
+            dlg.drawNew();
+            dlg.setCenter(world.center());
+        },
+        localize('Back...')
+    );
+    btn2.hide();
+    licenseBtn = dlg.addButton(
+        function () {
+            dlg.body.text = noticeTxt;
+            dlg.body.drawNew();
+            btn1.show();
+            btn2.show();
+            btn3.hide();
+            btn4.hide();
+            licenseBtn.hide();
+            translatorsBtn.hide();
+            dlg.fixLayout();
+            dlg.drawNew();
+            dlg.setCenter(world.center());
+        },
+        localize('License...')
+    );
+    btn3 = dlg.addButton(
+        function () {
+            dlg.body.text = versions;
+            dlg.body.drawNew();
+            btn1.show();
+            btn2.show();
+            btn3.hide();
+            btn4.hide();
+            licenseBtn.hide();
+            translatorsBtn.hide();
+            dlg.fixLayout();
+            dlg.drawNew();
+            dlg.setCenter(world.center());
+        },
+        localize('Modules...')
+    );
+    btn4 = dlg.addButton(
+        function () {
+            dlg.body.text = creditsTxt;
+            dlg.body.drawNew();
+            btn1.show();
+            btn2.show();
+            translatorsBtn.show();
+            btn3.hide();
+            btn4.hide();
+            licenseBtn.hide();
+            dlg.fixLayout();
+            dlg.drawNew();
+            dlg.setCenter(world.center());
+        },
+        localize('Credits...')
+    );
+    translatorsBtn.hide();
+    dlg.fixLayout();
+    dlg.drawNew();
 };
